0d63dd82556fb0706bf5e0713b4a33f52c0550bd
Nikhil Vishwakarma <121393371+nikhivishwaa@users.noreply.github.com>
2025-03-31 20:00:24 +0530

    fixing cors

--- aichallenge/settings.py
+++ aichallenge/settings.py
@@ -40,7 +40,6 @@ ALLOWED_HOSTS = []
 if os.environ.get('ALLOWED_HOSTS', ''):
     ALLOWED_HOSTS += os.environ.get('ALLOWED_HOSTS').split()
 
-CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split()
 
 # Application definition
 
@@ -65,7 +64,8 @@ EXTERNAL_APPS = [
 INSTALLED_APPS += EXTERNAL_APPS
 
 # CORS_ALLOW_ALL_ORIGINS = True 
-CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS','').split()
+CORS_ALLOW_ALL_ORIGINS = os.getenv('CORS_ALLOW_ALL_ORIGINS','') == 'TRUE'
+# CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS','').split()
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
